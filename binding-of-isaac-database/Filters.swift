096742034e45e1301bb796a148a9fa0aadcd5d82
Craig Holliday <hollidaycraig@yahoo.com>
2017-01-06 15:44:19 -0600

    added more items and fixed filtering

---
+++ binding-of-isaac-database/Filters.swift
@@ -0,0 +1,9 @@
+//
+//  Filters.swift
+//  binding-of-isaac-database
+//
+//  Created by Craig Holliday on 1/6/17.
+//  Copyright © 2017 Craig Holliday. All rights reserved.
+//
+
+import Foundation
